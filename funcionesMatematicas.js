ce54c90758f5fffeee37f004be310a52a2485967
Fidel Vasquez Carata <fido.egd2020@gmail.com>
2025-10-11 11:45:46 -0400

    Update funcionesMatematicas.js
    Ordenamiento

--- funcionesMatematicas.js
+++ funcionesMatematicas.js
@@ -11,12 +11,11 @@ Integrantes y funciones :
 - Fidel Vasquez Carata: suma
 - Pablo Walter Zuleta Novillo: resta
 - Jose Aaron Coaquira Jimenez: multiplicacion
-- (Nombre 4): division
 - Enrique Walter Zuleta Novillo: factorial de un numero
 - Julio Cesar Garcia Omonte: Potenciacion
 - Jherlan Marcelo Calero Sardan: Serie Fibonacci
 - Liz Karen Aroja Pinaya: radicacion
-- Erick Samuel Peñaloza Lujan
+- Erick Samuel Peñaloza Lujan: raiz cuadrada
 - Frank Saul Guarayo Colomi: esPrimo
 ===========================================
 */
@@ -27,78 +26,6 @@ Integrantes y funciones :
 function suma(a, b) {
   return a + b;
 }
-
-
-
-// === EXPORTS PARA USO EN NODE O TESTS  ===
-module.exports = {
-  suma,
-  resta,
-  multiplicacion,
-  factorial,
-  potenciacion,
-  fibonacci,
-  radicacion,
-  raizCuadradaDescompuesta,
-  esPrimo
-};
-
-// === Pruebas rapidas  ===
-if (require.main === module) {
-  console.log("La suma de estos numeros (2,3) es =", suma(2,3));          // tiene que dar 5
-  // colocar sus operaciones MATEMATICAS....
-}
-
-//Autor: Pablo Walter Zuleta Novillo
-function resta(a,b){
-  return a - b;
-};
-
-//Autor: Jose Aaron Coaquira Jimenez
-function multiplicacion(a, b){
-  return a * b;	
-};
-
-
-//Autor: Enrique Walter Zuleta Novillo
-
-function factorial(n){
-    if (n === 0 || n === 1) {
-      return 1;
-    }
-  
-    return n * factorial(n - 1);
-};
-
-//Autor: Julio Cesar Garcia Omonte
-function potenciacion(base,exponente){
-  return base**exponente;
-};
-
-//Autor: Jherlan Marcelo Calero Sardan
-function fibonacci(n){
-  if (n < 0){
-    return "El número debe ser positivo";
-  }
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-if (require.main === module) {
-  console.log("La resta de los numeros (2,3) es =", resta(4,2));          // tiene que dar 2
-  console.log("La multiplicacion de los numeros (5, 4) es = ", multiplicacion(5,4));         // tiene que dar 20
-  console.log("El factorial de 5 es = ", factorial(5));   // Tiene que dar 120
-  console.log("La potenciacion de los numeros, base 2 y exponente 4: (2,4) es= ",potenciacion(2,4)) //Tiene que dar 16
-  console.log("La serie Fibonacci del número 6 es =", fibonacci(6));      // 8
-  // Pruebas para la función de raíz cuadrada descompuesta
-  console.log(raizCuadradaDescompuesta(8));   // 2√2
-  console.log(raizCuadradaDescompuesta(12));  // 2√3
-  console.log(raizCuadradaDescompuesta(7));   // √7
-  console.log(raizCuadradaDescompuesta(25));  // 5
-  // colocar sus operaciones MATEMATICAS....
-
-};
 
 //Autor:Liz Karen Aroja Pinaya  
 function radicacion(base, indice) {
@@ -151,3 +78,70 @@ function division(a, b) {
   }
   return a / b;
 };
+
+//Autor: Pablo Walter Zuleta Novillo
+function resta(a,b){
+  return a - b;
+};
+
+//Autor: Jose Aaron Coaquira Jimenez
+function multiplicacion(a, b){
+  return a * b;	
+};
+
+
+//Autor: Enrique Walter Zuleta Novillo
+
+function factorial(n){
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+  
+    return n * factorial(n - 1);
+};
+
+//Autor: Julio Cesar Garcia Omonte
+function potenciacion(base,exponente){
+  return base**exponente;
+};
+
+//Autor: Jherlan Marcelo Calero Sardan
+function fibonacci(n){
+  if (n < 0){
+    return "El número debe ser positivo";
+  }
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+// === EXPORTS PARA USO EN NODE O TESTS  ===
+module.exports = {
+  suma,
+  resta,
+  multiplicacion,
+  factorial,
+  potenciacion,
+  fibonacci,
+  radicacion,
+  raizCuadradaDescompuesta,
+  esPrimo
+};
+
+// === Pruebas rapidas  ===
+if (require.main === module) {
+  console.log("La suma de estos numeros (2,3) es =", suma(2,3));          // tiene que dar 5
+  console.log("La resta de los numeros (2,3) es =", resta(4,2));          // tiene que dar 2
+  console.log("La multiplicacion de los numeros (5, 4) es = ", multiplicacion(5,4));         // tiene que dar 20
+  console.log("El factorial de 5 es = ", factorial(5));   // Tiene que dar 120
+  console.log("La potenciacion de los numeros, base 2 y exponente 4: (2,4) es= ",potenciacion(2,4)) //Tiene que dar 16
+  console.log("La serie Fibonacci del número 6 es =", fibonacci(6));      // 8
+  // Pruebas para la función de raíz cuadrada descompuesta
+  console.log(raizCuadradaDescompuesta(8));   // 2√2
+  console.log(raizCuadradaDescompuesta(12));  // 2√3
+  console.log(raizCuadradaDescompuesta(7));   // √7
+  console.log(raizCuadradaDescompuesta(25));  // 5
+}
+
+
